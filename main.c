7b95afda1fae2dd750c3b338029c279492e8e863
Boyang Han <yqszxx@gmail.com>
2022-01-25 13:28:47 +0800

    Test code for printf

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdint.h>
+
+uint64_t readMcycle() {
+    unsigned long mcycle;
+    asm volatile ("csrr %0, mcycle" : "=r"(mcycle));
+    return mcycle;
+}
 
 int main() {
     printf("Hello Newlib!\r\n");
+    printf("Current mcycle: %lu\r\n", readMcycle());
     return 0;
 }
